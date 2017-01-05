c3a6259087819558f2da1e67844c896eeeaf3231
Keith Dahlby <dahlbyk@gmail.com>
2017-01-05 13:22:08 -0600

    Add Chocolatey uninstall

--- chocolatey/tools/chocolateyUninstall.ps1
+++ chocolatey/tools/chocolateyUninstall.ps1
@@ -4,37 +4,26 @@
 
     try {
       if (test-path($poshgitPath)) {
-        Write-Host "Attempting to remove existing `'$poshgitPath`' prior to install."
+        Write-Host "Attempting to remove `'$poshgitPath`'."
         remove-item $poshgitPath -recurse -force
       }
     } catch {
       Write-Host "Could not remove `'$poshgitPath`'"
     }
 
-    $poshGitInstall = if($env:poshGit -ne $null){ $env:poshGit } else {'https://github.com/dahlbyk/posh-git/zipball/master'}
-    Install-ChocolateyZipPackage 'poshgit' $poshGitInstall $poshgitPath
-    $pgitDir = Dir "$poshgitPath\*posh-git*\" | Sort-Object -Property LastWriteTime | Select -Last 1
-
     if(Test-Path $PROFILE) {
         $oldProfile = @(Get-Content $PROFILE)
         $newProfile = @()
-        #If old profile exists replace with new one and make sure prompt preservation function is on top
-        $pgitExample = "$pgitDir\profile.example.ps1"
+
         foreach($line in $oldProfile) {
             if ($line -like '*PoshGitPrompt*') { continue; }
+            if ($line -like '*Load posh-git example profile*') { continue; }
+            if ($line -like '*posh-git*profile.example.ps1*') { continue; }
 
-            if($line.ToLower().Contains("$poshgitPath".ToLower())) {
-                $line = ". '$pgitExample'"
-            }
             $newProfile += $line
         }
         Set-Content -path $profile -value $newProfile -Force
     }
-
-    $subfolder = get-childitem $poshgitPath -recurse -include 'dahlbyk-posh-git-*' | select -First 1
-    write-debug "Found and using folder `'$subfolder`'"
-    $installer = Join-Path $subfolder 'install.ps1'
-    & $installer
 } catch {
   try {
     if($oldProfile){ Set-Content -path $PROFILE -value $oldProfile -Force }
